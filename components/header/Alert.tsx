ce2d3c14aa62a5cada7a480395910f2fafa684c1
EduardoRocky <luiz.veltroni@mediamonks.com>
2023-10-23 11:11:59 -0300

    feat: initial configs

--- components/header/Alert.tsx
+++ components/header/Alert.tsx
@@ -20,7 +20,7 @@ function Alert({ alerts = [], interval = 5 }: Props) {
         {alerts.map((alert, index) => (
           <Slider.Item index={index} class="carousel-item">
             <span class="text-sm text-secondary-content flex justify-center items-center w-screen h-[38px]">
-              {alert}
+              {alert}!!!
             </span>
           </Slider.Item>
         ))}
