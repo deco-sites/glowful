d9700e867289b8a885e23c46c1a34433af19bc7e
EduardoRocky <luiz.veltroni@mediamonks.com>
2023-10-25 09:04:24 -0300

    stykes: import style guide

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,5 +4,24 @@ export default {
   plugins: [daisyui],
   daisyui: { themes: [], logs: false },
   content: ["./**/*.tsx"],
-  theme: { container: { center: true } },
+  theme: {
+    container: { center: true },
+    fontFamily: {
+      redhat: ["Red Hat Display"],
+      fraunces: ["Fraunces"],
+    },
+    colors: {
+      "deep-beauty": "#101820",
+      "white-lily": "#FFFFFF",
+      "cherry-pop": "#CE0F69",
+      "candy-lips": "#D7A3AB",
+      "peach-skin": "#FFBE9F",
+      "glow-night": "#13294B",
+      "sereni-green": "#1C8172",
+      "soft-pink": "#FF8674",
+      "radiant-blue": "#9BCBEB",
+      "grape-juice": "#9063CD",
+      "sunny-day": "#FFC658",
+    },
+  },
 };
