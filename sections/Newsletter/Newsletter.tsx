93b5d23a52380bd4642658d74d0d88e6875ca153
EduardoRocky <luiz.veltroni@mediamonks.com>
2023-12-21 15:39:08 -0300

    refactor: change newsletter component

--- sections/Newsletter/Newsletter.tsx
+++ sections/Newsletter/Newsletter.tsx
@@ -4,27 +4,17 @@ import type { ImageWidget } from "apps/admin/widgets.ts";
 export interface Form {
   placeholder?: string;
   buttonText?: string;
-  /** @format html */
-  helpText?: string;
 }
 
 export interface Props {
-  /**
-   * @format html
-   */
   title?: string;
   /** @format textarea */
   description?: string;
+  /** @format color */
+  color?: string;
   form?: Form;
-  background?: ImageWidget;
-  layout?: {
-    headerFontSize?: "Large" | "Normal";
-    content?: {
-      border?: boolean;
-      alignment?: "Center" | "Left" | "Side to side";
-      bgColor?: "Normal" | "Reverse";
-    };
-  };
+  backgroundDesktop?: ImageWidget;
+  backgroundMobile?: ImageWidget;
 }
 
 const DEFAULT_PROPS: Props = {
@@ -33,102 +23,89 @@ const DEFAULT_PROPS: Props = {
   form: {
     placeholder: "Digite seu email",
     buttonText: "Inscrever",
-    helpText:
-      'Ao se inscrever, você concorda com nossa <a class="link" href="/politica-de-privacidade">Política de privacidade</a>.',
-  },
-  layout: {
-    headerFontSize: "Large",
-    content: {
-      border: false,
-      alignment: "Center",
-    },
   },
 };
 
 export default function Newsletter(props: Props) {
-  const { title, description, form, layout, background } = {
+  const {
+    title,
+    description,
+    form,
+    color,
+    backgroundDesktop,
+    backgroundMobile,
+  } = {
     ...DEFAULT_PROPS,
     ...props,
   };
-  const isReverse = layout?.content?.bgColor === "Reverse";
-  const bordered = Boolean(layout?.content?.border);
 
   const headerLayout = (
-    <Header
-      title={title}
-      description={description}
-      alignment={layout?.content?.alignment === "Left" ? "left" : "center"}
-      colorReverse={isReverse}
-      fontSize={layout?.headerFontSize}
-    />
+    <div class="flex flex-col gap-[24px] w-full">
+      <h3
+        class="text-[26px] font-frances font-normal leading-[120%] text-deep-beauty"
+        style={{ color: color ? color : "#101820" }}
+      >
+        {title}
+      </h3>
+      <p
+        class="leading-[150%] text-deep-beauty"
+        style={{ color: color ? color : "#101820" }}
+      >
+        {description}
+      </p>
+    </div>
   );
 
   const formLayout = form && (
-    <form action="/" class="flex flex-col gap-4">
-      <div class="flex flex-col lg:flex-row gap-4">
-        <input
-          class="input input-bordered lg:w-80 bg-[#FFFFFF40] border-2 border-white-lily placeholder:text-white-lily text-[16px] font-redhat text-[#fff]"
-          type="text"
-          placeholder={form.placeholder}
-        />
-        <button
-          class={`btn ${
-            isReverse ? "btn-accent" : ""
-          } px-[40px] border-0 text-white-lily text-uppercase text-[14px] tracking-[1px] font-medium bg-gradient-to-r from-[#CE0F69] to-[#FF9EBC] lg:border lg:border-white-lily lg:bg-[#FFFFFF40] lg:from-transparent lg:to-transparent lg:hover:bg-gradient-to-r lg:hover:from-[#CE0F69] lg:hover:to-[#FF9EBC] transition-colors duration-300 lg:hover:border-transparent`}
-          type="submit"
-        >
-          {form.buttonText}
-        </button>
-      </div>
-      {form.helpText && (
-        <div
-          class="text-sm text-white-lily lg:text-[12px]"
-          dangerouslySetInnerHTML={{ __html: form.helpText }}
-        />
-      )}
+    <form action="/" class="flex gap-[8px] w-full">
+      <input
+        class={`w-full border-b bg-transparent border-deep-beauty  pl-[8px] placeholder:text-[${
+          color ? color : "#101820"
+        }] text-[16px] text-deep-beauty`}
+        type="text"
+        placeholder={form.placeholder}
+        style={{
+          color: color ? color : "#101820",
+          borderColor: color ? color : "#101820",
+        }}
+      />
+      <button
+        class={`btn px-[24px] py-[14px] rounded-full border-0 text-white-lily uppercase text-[14px] tracking-[0.7px] font-semibold bg-deep-beauty hover:bg-white-lily hover:text-deep-beauty`}
+        style={{ backgroundColor: color ? color : "#101820" }}
+        type="submit"
+      >
+        {form.buttonText}
+      </button>
     </form>
   );
 
-  const bgLayout = isReverse
-    ? "bg-secondary text-secondary-content"
-    : "bg-transparent";
-
   return (
-    <div
-      class={`${
-        bordered
-          ? isReverse
-            ? "bg-secondary-content"
-            : "bg-secondary"
-          : bgLayout
-      } ${bordered ? "p-4 lg:p-16" : "p-0"} bg-cover bg-no-repeat bg-center`}
-      style={{ backgroundImage: `url("${background}")` }}
-    >
-      {(!layout?.content?.alignment ||
-        layout?.content?.alignment === "Center") && (
+    <>
+      {/* Desktop */}
+      <div
+        class={`hidden lg:block p-0bg-cover bg-no-repeat bg-center`}
+        style={{ backgroundImage: `url("${backgroundDesktop}")` }}
+      >
         <div
-          class={`container flex flex-col rounded p-4 gap-[40px] lg:p-16 lg:gap-12 ${bgLayout} `}
+          class={`container max-w-[1200px] flex flex-col rounded justify-between lg:items-end lg:flex-row px-[24px] py-[60px] gap-[40px] lg:py-[90px] lg:gap-[70px]`}
         >
           {headerLayout}
-          <div class="flex justify-center">{formLayout}</div>
+          {formLayout}
         </div>
-      )}
-      {layout?.content?.alignment === "Left" && (
+      </div>
+
+      {/* Mobile */}
+      <div
+        class={`block lg:hidden p-0bg-cover bg-no-repeat bg-center`}
+        style={{ backgroundImage: `url("${backgroundMobile}")` }}
+      >
         <div
-          class={`container flex flex-col rounded p-4 gap-[40px] lg:p-16 lg:gap-12 ${bgLayout}`}
+          class={`container max-w-[1200px] flex flex-col rounded justify-between lg:items-end lg:flex-row px-[24px] py-[60px] gap-[40px] lg:py-[90px] lg:gap-[70px] `}
         >
           {headerLayout}
-          <div class="flex justify-start">{formLayout}</div>
+          {formLayout}
         </div>
-      )}
-      {layout?.content?.alignment === "Side to side" && (
-        <div
-          class={`container flex flex-col rounded justify-between lg:items-center lg:flex-row p-4 gap-[40px] lg:p-16 lg:gap-12 ${bgLayout}`}
-        >
-          {headerLayout}
-          <div class="flex justify-center">{formLayout}</div>
-        </div>
-      )}
-    </div>
+      </div>
+    </>
   );
 }
