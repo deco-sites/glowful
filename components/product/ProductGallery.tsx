307ae2a8bc69dc6b3ddac1b371469ea842176a8d
EduardoRocky <luiz.veltroni@mediamonks.com>
2024-02-22 14:32:41 -0300

    fix: carousel product card and link cart

--- components/product/ProductGallery.tsx
+++ components/product/ProductGallery.tsx
@@ -38,7 +38,7 @@ function ProductGallery({ products, layout, offset, pageInfo }: Props) {
 
   return (
     <div
-      class={`relative grid ${mobile} pb-[80px] gap-x-[32px] gap-y-[80px] items-center justify-items-center sm:grid-cols-2 xl:grid-cols-3 before:w-full before:h-[1px] before:bg-[#E4E4E4] before:absolute before:bottom-0`}
+      class={`relative grid ${mobile} pb-[80px] mb-[80px] lg:mb-0 gap-x-[32px] gap-y-[80px] items-center justify-items-center sm:grid-cols-2 xl:grid-cols-3 before:w-full before:h-[1px] before:bg-[#E4E4E4] before:absolute before:bottom-0`}
     >
       {products?.map((product, index) => (
         <ProductCard
