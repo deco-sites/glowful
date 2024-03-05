27a89dfd9f2e8e13d71e01452571286411c57ae5
EduardoRocky <luiz.veltroni@mediamonks.com>
2024-03-05 15:15:04 -0300

    test: switch case for select widget

--- sections/Tools/Review.tsx
+++ sections/Tools/Review.tsx
@@ -2,17 +2,109 @@ import { useUI } from "../../sdk/useUI.ts";
 
 export interface Props {
   AllReviews?: boolean;
+  reviewStyle:
+    | "Product Review Widget"
+    | "Rating Widget"
+    | "Cards Carousel Widget"
+    | "Testimonials Carousel Widget"
+    | "Gallery Carousel Widget"
+    | "Popup Widget"
+    | "Sidebar Widget";
 }
 
-function Review({ AllReviews }: Props) {
+function Review({ AllReviews, reviewStyle }: Props) {
   const { productId } = useUI();
 
   const id = productId.value;
 
+  let reviewStyleSelected;
+
+  switch (reviewStyle) {
+    case "Product Review Widget":
+      reviewStyleSelected = <div id="looxReviews" data-product-id={id}></div>;
+      break;
+    case "Rating Widget":
+      reviewStyleSelected = (
+        <div class="loox-rating" data-fetch data-id={id}></div>
+      );
+      break;
+    case "Cards Carousel Widget":
+      reviewStyleSelected = (
+        <div id="loox-default-carousel">
+          <div
+            class="loox-v2-carousel-container"
+            id="LOOX-V2_CAROUSEL-card"
+            data-slide-type="card"
+          ></div>
+        </div>
+      );
+      break;
+    case "Testimonials Carousel Widget":
+      reviewStyleSelected = (
+        <div id="loox-default-carousel">
+          <div
+            class="loox-v2-carousel-container"
+            id="LOOX-V2_CAROUSEL-testimonial"
+            data-slide-type="testimonial"
+          ></div>
+        </div>
+      );
+      break;
+    case "Gallery Carousel Widget":
+      reviewStyleSelected = (
+        <div id="loox-default-carousel">
+          <div
+            class="loox-v2-carousel-container"
+            id="LOOX-V2_CAROUSEL-gallery"
+            data-slide-type="gallery"
+          ></div>
+        </div>
+      );
+      break;
+    case "Popup Widget":
+      reviewStyleSelected = (
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        var loox_pop_active = {
+          true;
+        };
+        var loox_pop_display = {
+          "other_pages":true
+        };
+      `,
+          }}
+        ></script>
+      );
+      break;
+    case "Sidebar Widget":
+      reviewStyleSelected = (
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        var loox_floating_widget = {
+          active: true,
+          display_on_other_pages: true,
+          position: "left",
+          button_text: "Reviews",
+          button_bg_color: "333333",
+          button_text_color: "FFFFFF",
+          hide_on_mobile: false,
+          display_on_home_page: true,
+        };
+      `,
+          }}
+        ></script>
+      );
+      break;
+    default:
+      reviewStyleSelected = <div id="looxReviews" data-loox-aggregate></div>;
+  }
+
   return (
     <>
       <script
-        async
+        defer
         src="//loox.io/widget/loox.js?shop=20c805-5.myshopify.com"
       />
 
@@ -20,7 +112,7 @@ function Review({ AllReviews }: Props) {
         {AllReviews === true ? (
           <div id="looxReviews" data-loox-aggregate></div>
         ) : (
-          <div id="looxReviews" data-product-id={id}></div>
+          reviewStyleSelected
         )}
       </div>
     </>
