0841a6b1c3f050b9e8dcc20c46f866f788dda2bf
Alison Leme <alisonledos@gmail.com>
2024-03-19 16:28:36 -0300

    fix: width videowise

--- sections/VideoWise/VideoWise.tsx
+++ sections/VideoWise/VideoWise.tsx
@@ -1,6 +1,6 @@
 function VideoWise() {
   return (
-    <div class="container py-[64px]">
+    <div class="py-[64px]">
       <div
         class="reeview-app-widget"
         id="reeview-app-widget_65f444fcdb33eb002981be8a"
