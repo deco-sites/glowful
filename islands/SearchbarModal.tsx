c5d0654571c9f0ff720f79083d38fb46d7da6fd0
EduardoRocky <luiz.veltroni@mediamonks.com>
2024-03-25 15:43:43 -0300

    styles: fix margin top display top

--- islands/SearchbarModal.tsx
+++ islands/SearchbarModal.tsx
@@ -4,6 +4,7 @@ import Searchbar, {
 } from "$store/components/search/Searchbar.tsx";
 import Modal from "$store/components/ui/Modal.tsx";
 import { useUI } from "$store/sdk/useUI.ts";
+import { useEffect, useState } from "preact/hooks";
 
 export interface Props {
   searchbar?: SearchbarProps;
@@ -12,18 +13,20 @@ export interface Props {
 function SearchbarModal({ searchbar }: Props) {
   const { displaySearchPopup, displayTop } = useUI();
   const pathname = window.location.pathname;
+  const newNavbarHeight = Number(navbarHeight.split("px")[0]);
+  const [heightCalc, setHeightCalc] = useState(newNavbarHeight);
 
   if (!searchbar) {
     return null;
   }
 
-  let heightCalc = navbarHeight;
-
-  if (pathname === "/") {
-    heightCalc = displayTop ? `calc(${navbarHeight} + 40px` : navbarHeight;
-  } else {
-    heightCalc = navbarHeight;
-  }
+  useEffect(() => {
+    if (pathname === "/") {
+      setHeightCalc(displayTop.value ? newNavbarHeight + 40 : newNavbarHeight);
+    } else {
+      setHeightCalc(newNavbarHeight);
+    }
+  }, [displayTop.value]);
 
   return (
     <Modal
@@ -34,7 +37,7 @@ function SearchbarModal({ searchbar }: Props) {
       <div
         class="absolute top-0 bg-base-100 container lg:h-fit lg:!max-w-[1130px]"
         style={{
-          marginTop: heightCalc,
+          marginTop: `${heightCalc}px`,
         }}
       >
         <Searchbar {...searchbar} />
