03b49826685f8997ffbd91daeb644d0536e96b22
pandeiro7 <rafasoli10@gmail.com>
2024-04-04 15:32:20 -0300

    fix: bug image

--- sections/Plural/IconAndText.tsx
+++ sections/Plural/IconAndText.tsx
@@ -30,8 +30,8 @@ export default function ImageAndText(props: Props) {
   };
 
   return (
-    <div class="lg:bg-[#F4F4F4]">
-      <div class="lg:px-0 lg:ml-[2%] card lg:card-side rounded grid grid-cols-1 justify-betweenx lg:grid-cols-[50%_48%] xl:grid-cols-[58%_40%] justify-items-center gap-2">
+    <div class="lg:bg-[#F4F4F4] xl:overflow-hidden">
+      <div class="lg:px-0 lg:ml-[2%] card lg:card-side rounded grid grid-cols-1 justify-betweenx lg:grid-cols-[50%_48%] xl:grid-cols-[58%_42%] justify-items-center gap-2">
         <figure class=" relative items-center justify-centera justify-self-end max-h-[750px] ">
           <Picture class="hidden lg:flex justify-center">
             <img
@@ -45,7 +45,7 @@ export default function ImageAndText(props: Props) {
           </Picture>
         </figure>
 
-        <div class="lg:py-10 2xl:py-20 px-[24px] lg:px-0 flex flex-col items-center gap-[28px] lg:gap-[80px] lg:row-start-1 lg:col-start-1 lg:max-w-[616px]">
+        <div class="lg:py-10 2xl:py-20 px-[24px] lg:px-0 flex flex-col items-center gap-[28px] lg:gap-[80px] lg:row-start-1 lg:col-start-1 lg:max-w-[616px] 2xl:max-w-[716px]">
           <div
             class="card-title textIconHighLight text-[24px] tracking-[1.2px] uppercase font-medium lg:text-[32px] text-center justify-center"
             dangerouslySetInnerHTML={{ __html: title }}
@@ -69,7 +69,7 @@ export default function ImageAndText(props: Props) {
                   </div>
 
                   {/* DESKTOP */}
-                  <div class="hidden lg:flex items-start gap-6 lg:max-w-[260px]" style={{ width: "calc(50% - 1rem)" }}>
+                  <div class="hidden lg:flex items-start gap-6 lg:max-w-[260px] 2xl:max-w-[335px]" style={{ width: "calc(50% - 1rem)" }}>
                     <img src={benefit.image} width={32} height={32} alt="" />
                     <div class="flex flex-col gap-[16px]">
                       <p class="xl:text-[22px] font-semibold tracking-[1.1px] leading-[130%] text-deep-beauty text-base">
