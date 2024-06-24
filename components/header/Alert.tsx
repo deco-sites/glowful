b46ec04231e7a3a8d74c056ee59dfbfdf8dc807e
EduardoRocky <luiz.veltroni@mediamonks.com>
2024-06-24 14:36:00 -0300

    fix: padding in alert

--- components/header/Alert.tsx
+++ components/header/Alert.tsx
@@ -44,7 +44,7 @@ function Alert({ alerts = [], interval = 5 }: Props) {
     <div id={id} class={displayTop.value ? "" : "hidden"}>
       {alerts.map((alert, index) => (
         <Slider
-          class="carousel carousel-center w-full bg-deep-beauty gap-6 text-center p-[14px] lg:p-[16px] 2xl:p-[18px]"
+          class="carousel carousel-center w-full bg-deep-beauty gap-6 text-center p-[14px]"
           style={{ "background-color": alert.background }}
         >
           <Slider.Item index={index} class="carousel-item w-full">
